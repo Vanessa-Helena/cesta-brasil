aaad0282e469144e9567611a877d2c12a5ec5d8a
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-06-22 09:14:18 -0300

    Criaçao de Paginas de cadastro

---
+++ lib/cadastro_fisica.dart
@@ -0,0 +1,106 @@
+import 'package:cesta_brasil/home_page.dart';
+import 'package:flutter/material.dart';
+
+class CadastroPage extends StatefulWidget {
+  const CadastroPage({Key? key}) : super(key: key);
+
+  @override
+  _CadastroPageState createState() => _CadastroPageState();
+}
+
+class _CadastroPageState extends State<CadastroPage> {
+  String email = '';
+  String password = '';
+  String endereco = '';
+  String nascimento = '';
+  String name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  onChanged: (text) {
+                    name = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Nome Completo', border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  onChanged: (text) {
+                    email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'E-mail', border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  onChanged: (text) {
+                    nascimento = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Data de Nascimento',
+                      border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  onChanged: (text) {
+                    endereco = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Endereço Completo',
+                      border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  onChanged: (text) {
+                    password = text;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: 'Cadastre sua Senha',
+                      border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  onChanged: (text) {
+                    password = text;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: 'Repita sua Senha',
+                      border: OutlineInputBorder()),
+                ),
+                SizedBox(height: 10),
+                RaisedButton(
+                  onPressed: () {
+                    if (password != '' || email != '' || name != '') {
+                      Navigator.of(context).pushReplacementNamed('/home');
+                      print('ok');
+                    } else {
+                      print('Erro');
+                    }
+                  },
+                  child: Text('Cadastrar'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
